63ab3bbdab2b522ab36dded8289cc42786d3fb5f
Emma A. A. Rünzel <emma@ruenzel.de>
2023-11-12 15:40:48 +0100

    Add Basic Page Routes w/ Home, About & Contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
